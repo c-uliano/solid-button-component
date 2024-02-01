23581499301ef4fccbcf067efee5157bd8e3cb73
Colette Ulianogit config --global --listclear <culiano@live.com>
2024-01-31 19:43:14 -0500

    fontawesome installed

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,15 @@
 import { createSignal } from 'solid-js';
 import styles from './Button.module.scss';
 import { Variant, BRadius, Size, Color} from '~/types';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 export interface ButtonProps {
     text: string;
     variant?: Variant;
     color?: Color; // whatever you enter has to have a class by that same name to style it up
     size?: Size;
-    showIcon?: boolean;
+    icon?: IconProp;
     uppercase?: boolean;
     borderRadius?: BRadius;
     disabled?: boolean;
@@ -29,12 +31,12 @@ const Button = (props: ButtonProps) => {
         <>
             {props.href ? (
                 <a class={buttonObj}>
-                    {props.showIcon && <span class={styles.icon}>🚀</span>}
+                    {props.icon && <span class={styles.icon}>🚀</span>}
                     {props.text}
                 </a>
             ) : (
                 <button class={buttonObj} disabled={props.disabled}>
-                    {props.showIcon && <span class={styles.icon}>🚀</span>}
+                    {props.icon && <span class={styles.icon}>🚀</span>}
                     {props.text}
                 </button>
             )}
--- src/root.tsx
+++ src/root.tsx
@@ -13,6 +13,13 @@ import {
   Title,
 } from "solid-start";
 import "./root.scss";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+// This exports the whole icon packs for Brand and Solid.
+library.add(fas, fab, far);
 
 export default function Root() {
   return (
